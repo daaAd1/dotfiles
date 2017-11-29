a653f6c2d18dbad6f4139d20974ea9eb1f0c5f0e
Samuel Sepesi <samuel.sepesi@student.tuke.sk>
2017-11-29 17:18:18 +0100

    Small changes

--- zsh/.zsh/syntax.zsh
+++ zsh/.zsh/syntax.zsh
@@ -1,4 +1,3 @@
-#
 #█▓▒░ source the plugin https://github.com/zsh-users/zsh-syntax-highlighting
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,1 +1,2 @@
+export TERM=xterm
 for config (~/.zsh/*.zsh) source $config
